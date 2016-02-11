8728eed215f55ec8fd09b8a651682cfc206919af
sumedhjoshi <sumedh.m.joshi@gmail.com>
2016-02-11 13:36:30 -0500

    Added driver for 1D FDTD code.

---
+++ prototyping/drive_1D_inhomogenous.m
@@ -0,0 +1,29 @@
+
+
+% Set some constants.
+n = 2048;
+L = 500;
+x = linspace( -L, L, n );
+
+% Set the initial condition.
+sigma = 10;
+s0    = exp( 1 - x.^2 / sigma^2 );
+u0    = 0 * x;
+
+% Set some physical constants.
+c0   = 343;
+rho0 = 1.0;
+v0   = 100;
+
+% Set the time discretization.
+dt = ( x(2) - x(1) ) / c0 / 20;
+tf = 0.5;
+t  = linspace( 0, tf, round( tf / dt ) );
+
+% Solve the problem.
+data = fdtd_1D_inhomogenous( x, t, s0, u0, rho0, v0, c0 );
+
+% Plot the solution with arrival time lines.
+plot( data.x, data.S(:,end) );
+vline( t(end) * ( v0 + c0 ), 'r' );
+vline( - t(end) * ( c0 - v0 ), 'r' );
